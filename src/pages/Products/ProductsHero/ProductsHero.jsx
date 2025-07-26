a0feb423d182d699325f3b8da73aa2006a33f183
rajabovdavlat <rajabovoff1@gmail.com>
2025-07-26 16:26:43 +0500

    heropading+

--- src/pages/Products/ProductsHero/ProductsHero.jsx
+++ src/pages/Products/ProductsHero/ProductsHero.jsx
@@ -7,7 +7,7 @@ const ProductsHero = () => {
     <div className='container sm:py-[65px] py-[5px]'>
       <div className='flex'>
         <div>
-       <h2 className='w-full font-semibold text-4xl sm:text-5xl md:text-6xl sm:pt-20   pb-[35px] select-none'>Explore the Latest Trends and Timeless Classics</h2>
+       <h2 className='w-full font-semibold text-4xl sm:text-5xl md:text-6xl sm:pt-[70px] md:pt-[40px] pt-[20px]  mb-[35px] select-none'>Explore the Latest Trends and Timeless Classics</h2>
         <p className=' text-base sm:text-lg text-gray select-none'>Dive into the world of fashion excellence at StyleLoom. Our curated selection brings together the latest trends and timeless classics</p>
       </div>
         <img src={svgs.prheroimg} alt="" className='hidden md:block select-none auto-pulse-glow h-auto object-contain' />

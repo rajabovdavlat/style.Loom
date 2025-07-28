38e53686ff5f5c401fe98efbae911e5c5dae5ae4
rajabovdavlat <rajabovoff1@gmail.com>
2025-07-28 19:52:39 +0500

    contactHero+

--- src/pages/Contacts/Contacts.jsx
+++ src/pages/Contacts/Contacts.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import ContactsHero from './ContactsHero/ContactsHero';
 
 const Contacts = () => {
   return (
-    <div className='h-screen flex items-center justify-center bg-dark text-title '>
-      <div className='container'>
-        <h2 className='text-3xl'>Contacts</h2>
+    <div className='bg-dark text-title'>
+      <div>
+        <ContactsHero />
+
       </div>
     </div>
   );
---
+++ src/pages/Contacts/ContactsHero/ContactsHero.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { ShoppingCart, PackageCheck } from 'lucide-react';
+
+const ContactsHero = () => {
+  return (
+    <div className="container py-10">
+      <div className="flex flex-col gap-6 sm:gap-10 md:flex-row justify-between items-start md:items-center">
+        {/* Left  */}
+        <div>
+          <h2 className="text-white font-semibold text-3xl sm:text-4xl md:text-6xl leading-tight select-none">
+            Elegant Evening Gown
+          </h2>
+          <div className="flex flex-col sm:flex-row sm:items-center gap-2 sm:gap-6 mt-3">
+            <p className="text-base sm:text-lg text-gray-400 select-none">
+              Fitted bodice, flowing skirt
+            </p>
+            <span className="text-green-500 text-sm sm:text-base font-medium bg-green-100 px-3 py-1 rounded-full">
+              In stock
+            </span>
+          </div>
+        </div>
+
+        {/* right  */}
+        <div className="flex flex-col sm:flex-row gap-3 sm:gap-4 mt-6 md:mt-0">
+          {/* btn Add to cart */}
+          <button className="flex items-center justify-center gap-2 bg-[#1F1F1F] text-white px-5 py-2 rounded-md text-sm sm:text-base">
+            <ShoppingCart size={18} />
+            Add To Cart
+          </button>
+
+          {/* btn Shop now */}
+          <button className="flex items-center justify-center gap-2 bg-[#AB9C87] text-black px-5 py-2 rounded-md text-sm sm:text-base">
+            <PackageCheck size={18} />
+            Shop Now
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ContactsHero;
+
